9b9c362767e4a424df603de968972d978ce3320e
Woke <66000596+woke999@users.noreply.github.com>
2022-01-01 11:03:48 -0500

    Create Data.cs

---
+++ Core/Misc/Data.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pwnd.Core.Handler.Misc
+{
+    public static class Data
+    {
+        public static string user;
+        public static string password;
+        public static string Ver1 = "";
+
+        public static bool antiafk;
+        public static bool spam;
+
+        public static string ConvertedPercent;
+
+        //KEYS//
+        public static string key1 = ";
+        public static string key2 = "";
+    }
+}
